b6ad67f227c3049ac7742957165c3b1ed04470c9
crosenboom <69617125+crosenboom@users.noreply.github.com>
2021-10-13 13:55:50 -0500

    Consolidated Character Controller and Jump Improvement
    I consolidated all the programmers character controllers and improved the jump mechanics

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,47 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//Caleb Rosenboom
+//Caleb Rosenboom, Eric Dundas, Robert Spatz
+//Consolidated by Caleb Rosenboom
 //made using  https://craftgames.co/unity-2d-platformer-movement/ and https://www.youtube.com/watch?v=QGDeafTx5ug
 public class PlayerController : MonoBehaviour
 {
-    public float speed;
-    public float jumpForce;
-    public float PlayerMovement;
+    public float speed = 10;
+    public float inputHorizontal;
+
+
+    public float jumpForce = 5.5f;
+    private int extraJumps;
+    public int extraJumpsValue = 1;
+    public float fallMultiplier = 1.25f;
+    public float lowJumpMultiplier = 0.5f;
 
     private Rigidbody2D rb;
+    public float rememberGroundedFor = 0.1f;
+    float lastTimeGrounded;
     private bool facingRight = true;
-    //
     private bool isGrounded;
-    public Transform IsGroundChecker;
-    public float GroundCheckRadius;
-    public LayerMask whatIsGround;
+    public Transform isGroundedCheck;
+    public float checkGroundRadius = 0.25f;
+    public LayerMask groundLayer;
 
-    private int extraJumps;
-    public int extraJumpsValue;
-
+    //Start called before first update
     void Start()
     {
         extraJumps = extraJumpsValue;
         rb = GetComponent<Rigidbody2D>();
     }
-    //Controls player movement
-   void FixedUpdate()
+
+    void Update()
     {
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, GroundCheckRadius, whatIsGround);
-        PlayerMovement = Input.GetAxisRaw("Horizontal");
-        Debug.Log(PlayerMovement);
-        rb.velocity = new Vector2(PlayerMovement * PlayerSpeed, rb.velocity.y);
-        //
-        if(facingRight == false && PlayerMovement > 0)
+        Move();
+        Jump();
+        groundCheck();
+        jumpImprovement();
+    }
+    //Controls player movement
+    void Move()
+    {
+        inputHorizontal = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(inputHorizontal * speed, rb.velocity.y);
+        if (facingRight == false && inputHorizontal > 0)
         {
             Flip();
-        } else if (facingRight == true && PlayerMovement < 0)
+        }
+        else if (facingRight == true && inputHorizontal < 0)
         {
             Flip();
         }
     }
-    //Makes sure character is facing correct way when pushing the arrow keys
+
+    //Flips character
     void Flip()
     {
         facingRight = !facingRight;
@@ -50,20 +63,49 @@ public class PlayerController : MonoBehaviour
         transform.localScale = Scaler;
     }
 
-    //Controls Jumping 
-    void Update()
+    //Controls jumping
+    void Jump()
     {
-        if (isGrounded == true)
+        if (isGrounded)
         {
             extraJumps = extraJumpsValue;
         }
-        if(Input.GetKeyDown(KeyCode.UpArrow) && extraJumps > 0)
+        if (Input.GetButtonDown("Jump") && (isGrounded || Time.time -lastTimeGrounded <= rememberGroundedFor || extraJumps > 0))
+
         {
-            rb.velocity = Vector2.up * JumpForce;
+            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
             extraJumps--;
-        }else if (Input.GetKeyDown(KeyCode.UpArrow) && extraJumps == 0 && isGrounded == true)
+        }
+    }
+
+    //checks if player is Grounded
+    void groundCheck()
+    {
+        Collider2D collider = Physics2D.OverlapCircle(isGroundedCheck.position, checkGroundRadius, groundLayer);
+        if (collider != null)
         {
-            rb.velocity = Vector2.up * JumpForce;
+            isGrounded = true;
+        }
+        else
+        {
+            if (isGrounded)
+            {
+                lastTimeGrounded = Time.time;
+            }
+            isGrounded = false;
+        }
+    }
+
+    //This improves the feeling of jump
+    void jumpImprovement()
+    {
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rb.velocity.y > 0 && Input.GetButton("Jump"))
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity * (lowJumpMultiplier - 1) * Time.deltaTime;
         }
     }
 }

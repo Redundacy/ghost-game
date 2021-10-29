67a67f2c2ae3f1a56c9d09b28e37f78e2b273b73
Eric Dundas <kuribodragon@gmail.com>
2021-10-28 20:36:02 -0500

    Fancified the book throwing

--- Assets/Scripts/Caleb Scripts/BookBehavior.cs
+++ Assets/Scripts/Caleb Scripts/BookBehavior.cs
@@ -6,27 +6,49 @@ public class BookBehavior : MonoBehaviour
 {
     public float speed = 4.5f;
     public float platformTime;
+    public float maxDistance;
+    private Vector3 startingPosition;
+    private bool thrown = false;
+    private bool falling = false;
+
+    void Start() {
+        IEnumerator coroutine = Timer();
+        StartCoroutine(coroutine);
+        startingPosition = transform.position;
+    }
 
     void Update()
     {
         Throwing();
-        Timer();
     }
    
     private void Throwing()
     {
-        transform.position += transform.right * Time.deltaTime * speed;
-        platformTime = 4;
+        if (Mathf.Abs(transform.position.x - startingPosition.x) <= maxDistance) {
+            transform.position += transform.right * Time.deltaTime * speed;
+            if (!thrown) {
+                thrown = true;
+                GetComponent<Rigidbody2D>().AddForce(Vector2.up * 5, ForceMode2D.Impulse);
+            }
+        }
+        else {
+            if (!falling) {
+                GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+            }
+        }
+        
+        // platformTime = 4;
     }
 
 
-    private void Timer()
-    {
-        if(platformTime > 0)
-        {
-            platformTime -= Time.deltaTime;
-        }else
-        {
+    private IEnumerator Timer() {
+        yield return new WaitForSeconds(platformTime);
+        falling = true;
+        GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.layer == 8) {
             Destroy(gameObject);
         }
     }
